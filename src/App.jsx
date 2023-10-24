629716d639a1bff5499919f5cab734093225b575
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-10-24 14:47:56 -0700

    contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header";
 import LandingSection from "./components/LandingSection";
+import ProjectSection from "./components/ProjectSection";
 
 function App() {
   return (
     <>
       <Header />
       <LandingSection />
+      <ProjectSection />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack>
+      <Box borderWidth="1px" borderRadius="lg" bg="white" p={2} color="black">
+        <VStack space={1}>
+          <Image src={imageSrc} alt={title} />
+          <Heading size="md">{title}</Heading>
+          <Text>{description}</Text>
+          <Text>
+            See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
+          </Text>
+        </VStack>
+      </Box>
+    </VStack>
+  );
 };
 
 export default Card;
